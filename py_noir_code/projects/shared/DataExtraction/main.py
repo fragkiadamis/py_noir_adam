490d7794d972418c0bd1b53f410da6abfd5e28fe
Duckflip <guewen.hubert@inria.fr>
2025-03-21 12:55:41 +0100

    Update

--- py_noir_code/projects/shared/DataExtraction/main.py
+++ py_noir_code/projects/shared/DataExtraction/main.py
@@ -1,4 +1,9 @@
 import json
+import sys
+import os
+
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../../')))
 
 from requests import Response
 from py_noir_code.src.API.api_service import post
@@ -7,8 +12,8 @@ from py_noir_code.src.utils.file_utils import get_ids_from_file
 
 
 def init_extraction(ids : [], resultOnly: bool):
-    #response = post("/datasets/datasetProcessing/massiveDownloadByProcessingIds", params = {"resultOnly":"true" if resultOnly else "false"}, data = json.dumps(ids))
-    response = post("/datasets/datasetProcessing/massiveDownloadProcessingByExaminationIds", params = {"processingComment":"comete_moelle/0.1", "resultOnly":"true" if resultOnly else "false"}, data = json.dumps(ids))
+    response = post("/datasets/datasetProcessing/massiveDownloadByProcessingIds", params = {"resultOnly":"true" if resultOnly else "false"}, data = json.dumps(ids))
+    #response = post("/datasets/datasetProcessing/massiveDownloadProcessingByExaminationIds", params = {"processingComment":"comete_moelle/0.1", "resultOnly":"true" if resultOnly else "false"}, data = json.dumps(ids))
     if response.status_code == 200 :
         start_download(response)
     else :
@@ -16,7 +21,7 @@ def init_extraction(ids : [], resultOnly: bool):
 
 def start_download(response : Response):
     if response.content.__len__() > 100 :
-        with open("comete_moelle_in_out_extraction.zip", "wb") as file:
+        with open("processing_in_out_extraction.zip", "wb") as file:
             for chunk in response.iter_content(chunk_size=8192):  # Download in chunks
                 file.write(chunk)
         print("Download completed (.zip is in your current directory) !")
@@ -25,4 +30,4 @@ def start_download(response : Response):
 
 if __name__ == '__main__':
     load_context("context.conf", False)
-    init_extraction(get_ids_from_file("processing_ids_to_extract.txt"), False)
+    init_extraction(get_ids_from_file("processing_ids_to_extract.txt"), True)
