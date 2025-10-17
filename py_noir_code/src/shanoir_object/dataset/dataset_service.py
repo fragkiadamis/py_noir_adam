89ca63053518751c679da2c9580224b713989aa7
Adam Fragkiadakis <adamfrag@gmail.com>
2025-10-17 17:06:02 +0200

    Refactor shanoir object

--- py_noir_code/src/shanoir_object/dataset/dataset_service.py
+++ py_noir_code/src/shanoir_object/dataset/dataset_service.py
@@ -161,3 +161,44 @@ def download_dataset_by_subject_id_study_id(subject_id, study_id, file_format, o
     dataset_ids = find_dataset_ids_by_subject_id_study_id(subject_id, study_id)
     download_datasets(dataset_ids, file_format, output_folder)
     return
+
+
+def get_dataset_processing(dataset_processing_id: str):
+    """ Get dataset processing [dataset_processing_id]
+    :param dataset_processing_id:
+    :return: json
+    """
+
+    path = ENDPOINT + '/datasetProcessing' + dataset_processing_id
+    response = get(path)
+    return response.json()
+
+
+def download_dataset_processing(dataset_processing_ids, output_folder, result_only=False, unzip=False):
+    """ Download datasets [dataset_ids] as [file_format] into [output_folder]
+    :param dataset_processing_ids:
+    :param output_folder:
+    :param result_only:
+    :param unzip:
+    :return:
+    """
+    if len(dataset_processing_ids) > 50:
+        logger.error('Cannot download more than 50 datasets at once. Please use the --search_text option instead to download '
+              'the datasets one by one.')
+        return
+    logger.info('Downloading dataset processing %s' % dataset_processing_ids)
+    path = ENDPOINT + '/datasetProcessing' + '/massiveDownloadByProcessingIds'
+    params = dict(resultOnly=str(result_only).lower())
+    response = post(path, params=params, json=dataset_processing_ids, stream=True)
+    download_file(output_folder, response, unzip=unzip)
+    return
+
+
+def find_processed_dataset_ids_by_input_dataset_id(dataset_id):
+    """ Get all processed datasets from dataset [dataset_id]
+    :param dataset_id:
+    :return:
+    """
+    path = ENDPOINT + '/datasetProcessing' + '/inputDataset/' + dataset_id
+    response = get(path)
+    return response.json()
